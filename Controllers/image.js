41d9262477f044be6ec9b3c81662aba9237c385f
sahilramteke4 <59560511+sahilramteke4@users.noreply.github.com>
2020-06-23 21:50:17 +0530

    Initializing local git repo for smartbrain API and adding source code.

---
+++ Controllers/image.js
@@ -0,0 +1,35 @@
+const Clarifai = require('clarifai');
+
+const app = new Clarifai.App({
+ apiKey: '7986ac8095ce425981abec43dff7e001'
+});
+
+const handleApiCall = (req, res) => {
+	app.models
+    .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+    .then(data => {
+    	res.json(data);
+    })
+    .catch(err => res.status(400).json('Unable to work with API'));
+}
+
+const handleImage = (req, res, db) => {
+	const { id } = req.body;
+	db('users')
+	  .where('id', '=', id)
+	  .increment('entries', 1)
+	  .returning('entries')
+	  .then(entries => {
+	  	if(entries.length) {
+	  	res.json(entries[0]);
+	  } else {
+	  	res.status(400).json('User does not exists');
+	  }
+	  })
+	  .catch(err => res.status(400).json('Unable to get entries'));
+}
+
+module.exports = {
+	handleImage: handleImage,
+	handleApiCall: handleApiCall
+}
---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+var knex = require('knex');
+
+const register = require('./Controllers/register');
+const signin = require('./Controllers/signin');
+const image = require('./Controllers/image');
+const profile = require('./Controllers/profile');
+
+const db = knex({
+  client: 'pg',
+  connection: {
+    host : '127.0.0.1',
+    user : 'postgres',
+    password : 'test',
+    database : 'smartbrain'
+  }
+});
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.get( '/', (req, res) => { res.send(database.users); })
+app.post( '/signin', (req, res) => {signin.handleSignin(req, res, db, bcrypt)} );
+app.post('/register', (req, res) => {register.handleRegister(req, res, db, bcrypt)} );
+app.get('/profile/:id', (req, res) => {profile.handleProfile(req, res, db)} );
+app.put('/image', (req, res) => {image.handleImage(req, res, db)} );
+app.post('/imageUrl', (req, res) => {image.handleApiCall(req, res)} );
+
+app.listen(3000, () => {
+	console.log('I am working at localhost:3000');
+});
