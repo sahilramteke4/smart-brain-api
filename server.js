bfab8358380f21fe64fb9631930d0aba0f4630a5
sahilramteke4 <59560511+sahilramteke4@users.noreply.github.com>
2020-06-29 21:11:40 +0530

    adding connection string for heroku postgres database

--- server.js
+++ server.js
@@ -11,10 +11,8 @@ const profile = require('./Controllers/profile');
 const db = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
-    user : 'postgres',
-    password : 'test',
-    database : 'smartbrain'
+    connectionString : process.env.DATABASE_URL,
+    ssl : true
   }
 });
 
