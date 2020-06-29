05850405cd8d1c71abc4c0c38161c653f8aa4e6f
sahilramteke4 <59560511+sahilramteke4@users.noreply.github.com>
2020-06-29 21:38:20 +0530

    clarifai.FACE_DETECT_MODEL replaced with a new key

--- Controllers/image.js
+++ Controllers/image.js
@@ -7,7 +7,7 @@ const app = new Clarifai.App({
 const handleApiCall = (req, res) => {
 	console.log('I am in image URL api');
 	app.models
-    .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+    .predict('c0c0ac362b03416da06ab3fa36fb58e3', req.body.input)
     .then(data => {
     	res.json(data);
     })
